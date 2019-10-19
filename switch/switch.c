ca68f994a6a4fb9475650b8feed1bb61c3fcaa1f
Mutahar-Majid <Mutahar-Majid@github.com>
2019-10-19 22:00:26 +0530

    modified main code to decrement chosen no. by 2 and added comments for changes

--- switch/switch.c
+++ switch/switch.c
@@ -6,17 +6,17 @@
 int main()
 {
     int num,ch;
-    printf("enter the  two digit number of your choice");
+    printf("enter the  two digit number of your choice\n");
     scanf("%d",&num);
-    while(ch!=2){
+    while(ch!=3){ //replaced ch = 2 with ch = 3
     printf("This is the following menu options\n");
     printf("----------1.Choose here for incrementing the number-----------------\n");
     printf("----------2.Choose here for decrementing the number-------------------\n");
     printf("----------3.EXIT THE PROGRAM----------------------------\n");
     printf("enter the choice");
     scanf("%d",&ch);
-    
-    switch(num) {
+
+    switch(ch) { // replaced num with ch
         case 1: {
             int res;
             res=incr(num);
@@ -26,17 +26,19 @@ int main()
         case 2:{
             int res;
             res=decr(num);
+            res=decr(res);//decremented twice 
             printf("the decremented number is %d\n",res);
             break;
-            
+
         }
         case 3:{
             exit(0);
             break;
         }
+
     }
-    }       
-    
+    }
+
 
     return 0;
 }
